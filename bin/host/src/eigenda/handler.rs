5aedb646d7d5b82b437b727b8547ad34cc23ff0b
PinelliaC <wylbzc4928@gmail.com>
2025-08-12 17:47:12 +0800

    refactor: update commitment verification logic in EigenDAChainHintHandler

--- bin/host/src/eigenda/handler.rs
+++ bin/host/src/eigenda/handler.rs
@@ -132,17 +132,16 @@ impl HintHandler for EigenDAChainHintHandler {
                     .push_witness(&blob)
                     .map_err(|e| anyhow!("eigen da blob push witness error {e}"))?;
 
-                // let last_commitment = witness.commitments.last().unwrap();
+                let last_commitment = EigenDABlobData::encode(blob.as_ref()).blob;
 
-                // make sure locally computed proof equals to returned proof from the provider
-                // TODO In fact, the calculation result following the EigenLayer approach is not the same as the cert blob info.
-                // if last_commitment[..32] != cert_blob_info.blob_header.commitment.x[..]
-                //     || last_commitment[32..64] != cert_blob_info.blob_header.commitment.y[..]
-                // {
-                //     return Err(
-                //         anyhow!("proxy commitment is different from computed commitment proxy",
-                //     ));
-                // };
+                if last_commitment[..32] != cert_blob_info.blob_header.commitment.x[..]
+                    || last_commitment[32..64] != cert_blob_info.blob_header.commitment.y[..]
+                {
+                    return Err(anyhow!(
+                        "proxy commitment is different from computed commitment proxy",
+                    ));
+                };
+
                 let proof: Vec<u8> = witness
                     .proofs
                     .iter()
